5e781782c3beeb4ceb8633a229038cd35f95499e
MarquesMiguel <miguelmcplacido@gmail.com>
2026-07-15 05:58:00 -0300

    create the fecth_html functions wich takes the url from the site (bookstoscrape) and returns the raw html, added the docstring

--- scrape.py
+++ scrape.py
@@ -6,3 +6,22 @@ from urllib.parse import urljoin
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+
+
+def fetch_html(url: str) -> str:
+    """
+    Return the fetched raw HTML
+
+    Parameters:
+        url (str): the url from the site to be fetched (https://books.toscrape.com)
+    
+    Returns:
+        res.text (str): the raw HTML if the request sucessed
+    """
+    session = requests.Session()    # for optimization
+
+
+    res = session.get(url, timeout=10)
+    res.encoding = 'utf-8'        # right encoding
+    return res.text
+
